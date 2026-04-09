3a801c4034e77428431df697bc50617f1978e6e7
ERCERCERCERCERC <2CharlesE@qegs.email>
2026-04-09 15:17:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -25,3 +25,21 @@ elif option == 4:
     print(num1*num2)
 else:
     print("u poopoo hed")
+
+#swapping if else for match
+print("welcome to cslc short for cslculator")
+option = int(input("enter 1 for +\nenter 2 for -\nenter 3 for /\nenter 4 for *\nGIMME NUMNUM: "))
+num1 = int(input("what be number of di first: "))
+num2 = int(input("give me yer two'th number: "))
+print("ta da. here be it. di ansuh o' t' kweschin is:")
+match option: 
+    case 1:
+        print(num1+num2)
+    case 2:
+        print(num1-num2)
+    case 3:
+        print(num1/num2)
+    case 4:
+        print(num1*num2)
+    case _:
+        print("gimme a reason not to cry")
