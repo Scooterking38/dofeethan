79c02a5ffe6f59aea9404725633140d03a08a2be
ERCERCERCERCERC <2CharlesE@qegs.email>
2026-05-21 15:51:15 +0100

    code checker with amazing humour just like its creator

---
+++ codecheck.py
@@ -0,0 +1,11 @@
+code = False
+while code != "az4h3":
+    code = input("put in a code pretty please hehehehehehe: \n")
+    if code == "az4h3":
+        print("right code buckaroo")
+    elif code == "ur mum":
+        print("AGH YOU POOFACE")
+    elif code == "why should i when you dont give me a code?":
+        print("ok then az4h3")
+    else:
+        print("please try again")
