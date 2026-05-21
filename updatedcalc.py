4e6b8503ac171599a51b356d3c325f643b075e82
ERCERCERCERCERC <2CharlesE@qegs.email>
2026-05-21 16:30:39 +0100

    Update updatedcalc.py

--- updatedcalc.py
+++ updatedcalc.py
@@ -1,20 +1,18 @@
-func = int(input("""welcome to calc,
+
+
+def calc(bum,num1,num2):
+    ops = "+-/*"
+    print(eval(f"{num1}{ops[bum-1]}{num2}"))
+
+
+func = int(input("""
 please select number based on what function you want to do.
 type 1 for addition
 type 2 for subtraction
 type 3 for division
 type 4 for multiplication
 """))
-num1 = int(input("please give yer first number"))
-num2 = int(input("please give yer sec'nd numb'r"))
-match func:
-    case 1:
-        print(num1+num2)
-    case 2:
-        print(num1-num2)
-    case 3:
-        print(num1/num2)
-    case 4:
-        print(num1*num2)
-    case _:
-        print("get a life and choose some proper numbers")
+num1 = float(input("please give yer first number"))
+num2 = float(input("please give yer sec'nd numb'r"))
+
+calc(func,num1,num2)
